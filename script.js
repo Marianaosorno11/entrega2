274d03fc49e3ffae792cdcc4da5e8b3e8f49809d
Marianaosorno11 <70726712+Marianaosorno11@users.noreply.github.com>
2021-02-07 21:26:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+
+var sp1=function()
+{
+var p1=document.getElementById("p1");
+var m1=document.getElementById("m1");
+var sn1=document.getElementById("sn1");
+
+sn1.autoplay=true;
+sn1.load();
+
+
+if ("sn1"=="sn1")
+{
+setTimeout(stop2,1000);
+}
+}
+var sp2=function()
+{
+var p2=document.getElementById("p2");
+var m2=document.getElementById("m2");
+var sn2=document.getElementById("sn2");
+
+sn2.autoplay=true;
+sn2.load();
+
+
+if ("sn2"=="sn2")
+{
+setTimeout(stop2,1000);
+
+}
+}
+var sp3=function()
+{
+var p3=document.getElementById("p3");
+var m3=document.getElementById("m3");
+var sn3=document.getElementById("sn3");
+
+sn3.autoplay=true;
+sn3.load();
+
+
+if ("sn3"=="sn3")
+{
+setTimeout(stop2,1000);
+}
+}
+var sp=function()
+{
+var p=document.getElementById("p");
+var m=document.getElementById("m");
+var sn=document.getElementById("sn");
+
+sn.autoplay=true;
+sn.load();
+
+
+if ("sn"=="sn")
+{
+setTimeout(stop2,1000);
+}
+}
